4e1afcfb9971cdc08599751f0314d39ca1a5f73b
Fliss Tournant <flisstee@hotmail.co.uk>
2016-07-13 14:32:07 +1200

    Update to Gtk StackSwitcher and addition of reverse complement function

--- bio-gtk-programme.py
+++ bio-gtk-programme.py
@@ -3,58 +3,108 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 #from Bio import Entrez
 from Bio import SeqIO
-from Bio import Seq
+from Bio.Seq import Seq
 from Bio.SeqUtils import GC
+from Bio.Alphabet import IUPAC
+
+
+class GcFrame(Gtk.Bin):
+    def __init__(self):
+        Gtk.Bin.__init__(self)
+
+        self.msg_label = Gtk.Label()
+        self.board_label = Gtk.Label("Bio")
+        self.entry = Gtk.Entry()
+        self.button = Gtk.Button("GC content")
+        self.button.connect("clicked", self.clicked_callback)
+        bbox = Gtk.HButtonBox()
+        bbox.add(self.button)
+        gc_box = Gtk.VBox()
+        gc_box.add(self.board_label)
+        gc_box.add(self.msg_label)
+        gc_box.add(self.entry)
+        self.entry.connect("activate", self.enter_callback)
+        gc_box.add(bbox)
+        self.add(gc_box)
+
+    def gc_content(self): #calculates gc % of sequence
+        entry_text = Seq(self.entry.get_text(), IUPAC.unambiguous_dna)
+        self.board_label.set_text("Type in your sequence")
+        gc_result = GC(entry_text)
+        self.msg_label.set_text(str(gc_result))
+
+    def clicked_callback(self, button): #runs function when button is clicked
+        self.gc_content()
+        
+    def enter_callback(self, widget): #runs function when enter pressed
+        self.gc_content()
+        
+class RcFrame(Gtk.Bin):
+    def __init__(self):
+        Gtk.Bin.__init__(self)
+
+        self.msg_label = Gtk.Label()
+        self.board_label = Gtk.Label("Bio")
+        self.entry = Gtk.Entry()
+        self.button = Gtk.Button("Reverse Complement")
+        self.button.connect("clicked", self.clicked_callback)
+        bbox = Gtk.HButtonBox()
+        bbox.add(self.button)
+        rc_box = Gtk.VBox()
+        rc_box.add(self.board_label)
+        rc_box.add(self.msg_label)
+        rc_box.add(self.entry)
+        self.entry.connect("activate", self.enter_callback)
+        rc_box.add(bbox)
+        self.add(rc_box)
+
+    def rev_comp(self): #gives the reverse complement of DNA sequence
+        entry_text = Seq(self.entry.get_text(), IUPAC.unambiguous_dna)
+        self.board_label.set_text("Type in your sequence")
+        rc_result = entry_text.reverse_complement()
+        self.msg_label.set_text(str(rc_result))
+
+    def clicked_callback(self, button): #runs function when button is clicked
+        self.rev_comp()
+        
+    def enter_callback(self, widget): #runs function when enter pressed
+        self.rev_comp()
+
 
 class MyWindow(Gtk.Window):
     
     def __init__(self):
         Gtk.Window.__init__(self)
         self.set_size_request(300, 100)
-        self.button1_connection = 0
         
         self.set_title("Bio Programme")
         self.msg_label = Gtk.Label()
         self.board_label = Gtk.Label("Bio")
         self.entry = Gtk.Entry()
-        self.button1 = Gtk.Button("GC content")
-        self.button1.connect("clicked", self.clicked_callback)
         
         vbox = Gtk.VBox()
-        bbox = Gtk.HButtonBox()
-        bbox.add(self.button1)
-        vbox.add(self.board_label)
-        vbox.add(self.msg_label)
-        vbox.add(self.entry)
-        self.entry.connect("activate", self.enter_callback)
-        vbox.add(bbox)
+        stack = Gtk.Stack()
+        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
+        stack.set_transition_duration(250)
+        switcher = Gtk.StackSwitcher()
+        switcher.set_stack(stack)
+        vbox.add(switcher)
+        vbox.add(stack)
+
+        gc_box = GcFrame()
+        rc_box = RcFrame()
+        
+        stack.add_titled(gc_box, "gc", "Calculate GC")
+        stack.add_titled(rc_box, "rc", "Reverse Complement")
+        
         self.add(vbox)
         
         self.show_all()
         self.connect("delete-event", self.on_quit)
+        
 
     def on_quit(self, widget, event):
         Gtk.main_quit()
-
-    def clicked_callback(self, button): #runs function when button is clicked
-        self.gc_content()
-
-    def enter_callback(self, widget): #runs function when enter pressed
-        self.gc_content()
-        
-    def run(self):
-        self.msg_label.set_text("What would you like to do?")
-        self.set_button_labels("GC content")   
-        self.enter_callback(Gtk.Entry, self.entry)
-                
-    def gc_content(self): #calculates gc % of sequence
-        entry_text = self.entry.get_text()
-        self.board_label.set_text("Type in your sequence")
-        result = GC(entry_text)
-        self.msg_label.set_text(str(result))
-
-
-        
 
        
 window = MyWindow()
