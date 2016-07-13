3648993323ec0213e5106400db5f437c277a0577
Fliss Tournant <flisstee@hotmail.co.uk>
2016-07-13 15:58:20 +1200

    Addition of translation and Entrez database functions

--- bio-gtk-programme.py
+++ bio-gtk-programme.py
@@ -1,7 +1,7 @@
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-#from Bio import Entrez
+from Bio import Entrez
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqUtils import GC
@@ -13,7 +13,7 @@ class GcFrame(Gtk.Bin):
         Gtk.Bin.__init__(self)
 
         self.msg_label = Gtk.Label()
-        self.board_label = Gtk.Label("Bio")
+        self.board_label = Gtk.Label("Type in your sequence")
         self.entry = Gtk.Entry()
         self.button = Gtk.Button("GC content")
         self.button.connect("clicked", self.clicked_callback)
@@ -29,8 +29,8 @@ class GcFrame(Gtk.Bin):
 
     def gc_content(self): #calculates gc % of sequence
         entry_text = Seq(self.entry.get_text(), IUPAC.unambiguous_dna)
-        self.board_label.set_text("Type in your sequence")
         gc_result = GC(entry_text)
+        self.board_label.set_text("Result")
         self.msg_label.set_text(str(gc_result))
 
     def clicked_callback(self, button): #runs function when button is clicked
@@ -44,7 +44,7 @@ class RcFrame(Gtk.Bin):
         Gtk.Bin.__init__(self)
 
         self.msg_label = Gtk.Label()
-        self.board_label = Gtk.Label("Bio")
+        self.board_label = Gtk.Label("Type in your sequence")
         self.entry = Gtk.Entry()
         self.button = Gtk.Button("Reverse Complement")
         self.button.connect("clicked", self.clicked_callback)
@@ -60,8 +60,8 @@ class RcFrame(Gtk.Bin):
 
     def rev_comp(self): #gives the reverse complement of DNA sequence
         entry_text = Seq(self.entry.get_text(), IUPAC.unambiguous_dna)
-        self.board_label.set_text("Type in your sequence")
         rc_result = entry_text.reverse_complement()
+        self.board_label.set_text("Result")
         self.msg_label.set_text(str(rc_result))
 
     def clicked_callback(self, button): #runs function when button is clicked
@@ -69,6 +69,75 @@ class RcFrame(Gtk.Bin):
         
     def enter_callback(self, widget): #runs function when enter pressed
         self.rev_comp()
+
+
+class TrFrame(Gtk.Bin):
+    def __init__(self):
+        Gtk.Bin.__init__(self)
+
+        self.msg_label = Gtk.Label()
+        self.board_label = Gtk.Label("Type in your sequence")
+        self.entry = Gtk.Entry()
+        self.button = Gtk.Button("Translate Sequence")
+        self.button.connect("clicked", self.clicked_callback)
+        bbox = Gtk.HButtonBox()
+        bbox.add(self.button)
+        tr_box = Gtk.VBox()
+        tr_box.add(self.board_label)
+        tr_box.add(self.msg_label)
+        tr_box.add(self.entry)
+        self.entry.connect("activate", self.enter_callback)
+        tr_box.add(bbox)
+        self.add(tr_box)
+
+    def translation(self): #translates sequence into protein
+        entry_text = Seq(self.entry.get_text(), IUPAC.unambiguous_dna)
+        mrna = entry_text.transcribe()
+        tr_result = mrna.translate()
+        self.board_label.set_text("Result")
+        self.msg_label.set_text(str(tr_result))
+
+    def clicked_callback(self, button): #runs function when button is clicked
+        self.translation()
+        
+    def enter_callback(self, widget): #runs function when enter pressed
+        self.translation()
+
+
+class EnFrame(Gtk.Bin):
+    def __init__(self):
+        Gtk.Bin.__init__(self)
+
+        self.msg_label = Gtk.Label()
+        self.board_label = Gtk.Label("Type in your id")
+        self.entry = Gtk.Entry()
+        self.button = Gtk.Button("Entrez")
+        self.button.connect("clicked", self.clicked_callback)
+        bbox = Gtk.HButtonBox()
+        #fbox = Gtk.FileChooserButton()
+        bbox.add(self.button)
+        en_box = Gtk.VBox()
+        en_box.add(self.board_label)
+        en_box.add(self.msg_label)
+        en_box.add(self.entry)
+        self.entry.connect("activate", self.enter_callback)
+        en_box.add(bbox)
+        #en_box.add(fbox)
+        self.add(en_box)
+
+    def entrez_db(self): #finds details from Entrez database
+        entry_text = Entrez.efetch(db = "nucleotide", id = [self.entry.get_text()], rettype = "fasta")
+        records = list(SeqIO.parse(entry_text, "fasta"))
+        en_result = (records)
+        self.board_label.set_text("Result")
+        self.msg_label.set_text(str(en_result))
+        SeqIO.write(records, "bio-gtk-entrez.txt", "fasta")
+
+    def clicked_callback(self, button): #runs function when button is clicked
+        self.entrez_db()
+        
+    def enter_callback(self, widget): #runs function when enter pressed
+        self.entrez_db()
 
 
 class MyWindow(Gtk.Window):
@@ -93,10 +162,13 @@ class MyWindow(Gtk.Window):
 
         gc_box = GcFrame()
         rc_box = RcFrame()
+        tr_box = TrFrame()
+        en_box = EnFrame()
         
         stack.add_titled(gc_box, "gc", "Calculate GC")
         stack.add_titled(rc_box, "rc", "Reverse Complement")
-        
+        stack.add_titled(tr_box, "tr", "Translate Sequence")
+        stack.add_titled(en_box, "en", "Entrez")
         self.add(vbox)
         
         self.show_all()
