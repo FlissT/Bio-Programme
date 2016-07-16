f43d02d2cdbad08d18721e6490825505f3b32525
Fliss Tournant <flisstee@hotmail.co.uk>
2016-07-16 15:43:41 +1200

    Update to using Glade and Gtk.Builder

--- bio-gtk-programme.py
+++ bio-gtk-programme.py
@@ -10,27 +10,26 @@ from Bio.Alphabet import IUPAC
 class LnFrame(Gtk.Bin):
     def __init__(self):
         Gtk.Bin.__init__(self)
-
-        self.msg_label = Gtk.Label()
-        self.board_label = Gtk.Label("Enter sequence")
-        self.entry = Gtk.Entry()
-        self.button = Gtk.Button("Sequence Length")
+        
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("seqlen-page-glade.glade")
+        self.sl_box = self.builder.get_object("SeqLen-box")
+        self.add(self.sl_box)
+        self.show_all()
+    
+        self.entry = self.builder.get_object("SeqLen-entry")
+        self.button = self.builder.get_object("SeqLen-button")
+        self.label = self.builder.get_object("SeqLen-result")
         self.button.connect("clicked", self.clicked_callback)
-        bbox = Gtk.HButtonBox()
-        bbox.add(self.button)
-        ln_box = Gtk.VBox()
-        ln_box.add(self.board_label)
-        ln_box.add(self.msg_label)
-        ln_box.add(self.entry)
         self.entry.connect("activate", self.enter_callback)
-        ln_box.add(bbox)
-        self.add(ln_box)
+        
 
     def seq_len(self): #gives the length of the sequence
-        entry_text = Seq(self.entry.get_text(), IUPAC.unambiguous_dna)
+        self.sl_entry = self.builder.get_object("SeqLen-entry")
+        entry_text = Seq(self.sl_entry.get_text(), IUPAC.unambiguous_dna)
         len_result = len(entry_text)
-        self.board_label.set_text("Result")
-        self.msg_label.set_text(str(len_result))
+        #self.board_label.set_text("Result")
+        self.label.set_text(str(len_result))
 
     def clicked_callback(self, button): #runs function when button is clicked
         self.seq_len()
@@ -43,30 +42,30 @@ class CmFrame(Gtk.Bin):
     def __init__(self):
         Gtk.Bin.__init__(self)
 
-        self.msg_label = Gtk.Label()
-        self.board_label = Gtk.Label("Enter sequences")
-        self.entry1 = Gtk.Entry()
-        self.entry2 = Gtk.Entry()
-        self.button = Gtk.Button("Sequence Comparison")
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("seqcomp-page-glade.glade")
+        self.cm_box = self.builder.get_object("SeqComp-box")
+        self.add(self.cm_box)
+        self.show_all()
+
+        self.entry1 = self.builder.get_object("SeqComp-entry1")
+        self.entry2 = self.builder.get_object("SeqComp-entry2")
+        self.button = self.builder.get_object("SeqComp-button")
+        self.label = self.builder.get_object("SeqComp-result")
         self.button.connect("clicked", self.clicked_callback)
-        bbox = Gtk.HButtonBox()
-        bbox.add(self.button)
-        cm_box = Gtk.VBox()
-        cm_box.add(self.board_label)
-        cm_box.add(self.msg_label)
-        cm_box.add(self.entry1)
-        cm_box.add(self.entry2)
+        
         self.entry1.connect("activate", self.enter_callback)
         self.entry2.connect("activate", self.enter_callback)
-        cm_box.add(bbox)
-        self.add(cm_box)
+
 
     def seq_comp(self): #compares two sequences
-        entry_text1 = Seq(self.entry1.get_text(), IUPAC.unambiguous_dna)
-        entry_text2 = Seq(self.entry2.get_text(), IUPAC.unambiguous_dna)
+        self.cm_entry1 = self.builder.get_object("SeqComp-entry1")
+        self.cm_entry2 = self.builder.get_object("SeqComp-entry2")
+        entry_text1 = Seq(self.cm_entry1.get_text(), IUPAC.unambiguous_dna)
+        entry_text2 = Seq(self.cm_entry2.get_text(), IUPAC.unambiguous_dna)
         comp_result = ((entry_text1) == (entry_text2))
-        self.board_label.set_text("Result")
-        self.msg_label.set_text(str(comp_result))
+        #self.board_label.set_text("Result")
+        self.label.set_text(str(comp_result))
 
     def clicked_callback(self, button): #runs function when button is clicked
         self.seq_comp()
@@ -79,26 +78,26 @@ class GcFrame(Gtk.Bin):
     def __init__(self):
         Gtk.Bin.__init__(self)
 
-        self.msg_label = Gtk.Label()
-        self.board_label = Gtk.Label("Enter sequence")
-        self.entry = Gtk.Entry()
-        self.button = Gtk.Button("GC content")
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("gc-page-glade.glade")
+        self.gc_box = self.builder.get_object("GC-box")
+        self.add(self.gc_box)
+        self.show_all()
+
+        self.entry = self.builder.get_object("GC-entry")
+        self.button = self.builder.get_object("GC-button")
+        self.label = self.builder.get_object("GC-result")
         self.button.connect("clicked", self.clicked_callback)
-        bbox = Gtk.HButtonBox()
-        bbox.add(self.button)
-        gc_box = Gtk.VBox()
-        gc_box.add(self.board_label)
-        gc_box.add(self.msg_label)
-        gc_box.add(self.entry)
         self.entry.connect("activate", self.enter_callback)
-        gc_box.add(bbox)
-        self.add(gc_box)
+        
 
     def gc_content(self): #calculates gc % of sequence
-        entry_text = Seq(self.entry.get_text(), IUPAC.unambiguous_dna)
+        self.gc_entry = self.builder.get_object("GC-entry")
+        entry_text = Seq(self.gc_entry.get_text(), IUPAC.unambiguous_dna)
         gc_result = GC(entry_text)
-        self.board_label.set_text("Result")
-        self.msg_label.set_text(str(gc_result))
+        #self.board_label.set_text("Result")
+        self.label.set_text(str(gc_result))
+        print(str(gc_result))
 
     def clicked_callback(self, button): #runs function when button is clicked
         self.gc_content()
@@ -109,27 +108,26 @@ class GcFrame(Gtk.Bin):
 class RcFrame(Gtk.Bin):
     def __init__(self):
         Gtk.Bin.__init__(self)
+        
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("revcomp-page-glade.glade")
+        self.rc_box = self.builder.get_object("RevComp-box")
+        self.add(self.rc_box)
+        self.show_all()
 
-        self.msg_label = Gtk.Label()
-        self.board_label = Gtk.Label("Enter sequence")
-        self.entry = Gtk.Entry()
-        self.button = Gtk.Button("Reverse Complement")
+        self.entry = self.builder.get_object("RevComp-entry")
+        self.button = self.builder.get_object("RevComp-button")
+        self.label = self.builder.get_object("RevComp-result")
         self.button.connect("clicked", self.clicked_callback)
-        bbox = Gtk.HButtonBox()
-        bbox.add(self.button)
-        rc_box = Gtk.VBox()
-        rc_box.add(self.board_label)
-        rc_box.add(self.msg_label)
-        rc_box.add(self.entry)
         self.entry.connect("activate", self.enter_callback)
-        rc_box.add(bbox)
-        self.add(rc_box)
+       
 
     def rev_comp(self): #gives the reverse complement of DNA sequence
-        entry_text = Seq(self.entry.get_text(), IUPAC.unambiguous_dna)
+        self.rc_entry = self.builder.get_object("RevComp-entry")
+        entry_text = Seq(self.rc_entry.get_text(), IUPAC.unambiguous_dna)
         rc_result = entry_text.reverse_complement()
-        self.board_label.set_text("Result")
-        self.msg_label.set_text(str(rc_result))
+        #self.board_label.set_text("Result")
+        self.label.set_text(str(rc_result))
 
     def clicked_callback(self, button): #runs function when button is clicked
         self.rev_comp()
@@ -142,27 +140,26 @@ class TrFrame(Gtk.Bin):
     def __init__(self):
         Gtk.Bin.__init__(self)
 
-        self.msg_label = Gtk.Label()
-        self.board_label = Gtk.Label("Enter sequence")
-        self.entry = Gtk.Entry()
-        self.button = Gtk.Button("Translate Sequence")
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("transl-page-glade.glade")
+        self.tr_box = self.builder.get_object("Transl-box")
+        self.add(self.tr_box)
+        self.show_all()
+
+        self.entry = self.builder.get_object("Transl-entry")
+        self.button = self.builder.get_object("Transl-button")
+        self.label = self.builder.get_object("Transl-result")
         self.button.connect("clicked", self.clicked_callback)
-        bbox = Gtk.HButtonBox()
-        bbox.add(self.button)
-        tr_box = Gtk.VBox()
-        tr_box.add(self.board_label)
-        tr_box.add(self.msg_label)
-        tr_box.add(self.entry)
         self.entry.connect("activate", self.enter_callback)
-        tr_box.add(bbox)
-        self.add(tr_box)
+        
 
     def translation(self): #translates sequence into protein
-        entry_text = Seq(self.entry.get_text(), IUPAC.unambiguous_dna)
+        self.tr_entry = self.builder.get_object("Transl-entry")
+        entry_text = Seq(self.tr_entry.get_text(), IUPAC.unambiguous_dna)
         mrna = entry_text.transcribe()
         tr_result = mrna.translate()
-        self.board_label.set_text("Result")
-        self.msg_label.set_text(str(tr_result))
+        #self.board_label.set_text("Result")
+        self.label.set_text(str(tr_result))
 
     def clicked_callback(self, button): #runs function when button is clicked
         self.translation()
@@ -175,34 +172,35 @@ class EnFrame(Gtk.Bin):
     def __init__(self):
         Gtk.Bin.__init__(self)
 
-        self.msg_label = Gtk.Label()
-        self.board_label = Gtk.Label("Enter ID")
-        self.entry = Gtk.Entry()
-        self.button = Gtk.Button("Entrez")
-        self.button.connect("clicked", self.clicked_callback)
-        bbox = Gtk.HButtonBox()
-        #fbox = Gtk.FileChooserButton()
-        #fbox.connect("file_set", self.on_file_selected)        
-        bbox.add(self.button)
-        en_box = Gtk.VBox()
-        en_box.add(self.board_label)
-        en_box.add(self.msg_label)
-        en_box.add(self.entry)
-        self.entry.connect("activate", self.enter_callback)
-        en_box.add(bbox)
-        #en_box.add(fbox)
-        self.add(en_box)
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("entrez-page-glade.glade")
+        self.en_box = self.builder.get_object("Entrez-box")
+        self.add(self.en_box)
+        self.show_all()
 
-    def on_file_selected(self, entry):
-        print("You chose ", entry.get_filename())
+        self.entry = self.builder.get_object("Entrez-entry")
+        self.button = self.builder.get_object("Entrez-button")
+        self.label = self.builder.get_object("Entrez-result")
+        self.button.connect("clicked", self.clicked_callback)
+        self.fbox = self.builder.get_object("Entrez-file")
+        #self.fbox.connect("file_set", self.on_file_selected)        
+       
+        self.entry.connect("activate", self.enter_callback)
+        
+
+    #def on_file_selected(self, entry):
+        #print("You chose ", self.entry.get_filename())
         
     def entrez_db(self): #finds details from Entrez database
+        self.tr_entry = self.builder.get_object("Entrez-entry")
         entry_text = Entrez.efetch(db = "nucleotide", id = [self.entry.get_text()], rettype = "fasta")
-        en_result = list(SeqIO.parse(entry_text, "fasta"))
-        self.board_label.set_text("Result")
-        self.msg_label.set_text(str("Written to file 'bio-gtk-entrez.txt'"))#en_result))
+        en_result = SeqIO.read(entry_text, "fasta")
+        #self.board_label.set_text("Result")
+        self.label.set_text(str("Written to file 'bio-gtk-entrez.txt'"))
         SeqIO.write(en_result, "bio-gtk-entrez.txt", "fasta")
-
+        print(en_result.id)
+        print(en_result.seq)
+        
     def clicked_callback(self, button): #runs function when button is clicked
         self.entrez_db()
         
@@ -220,7 +218,7 @@ class MyWindow(Gtk.Window):
         self.msg_label = Gtk.Label()
         self.board_label = Gtk.Label("Bio")
         self.entry = Gtk.Entry()
-        
+
         vbox = Gtk.VBox()
         stack = Gtk.Stack()
         stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
@@ -230,14 +228,14 @@ class MyWindow(Gtk.Window):
         vbox.add(switcher)
         vbox.add(stack)
 
-        ln_box = LnFrame()
+        sl_box = LnFrame()
         cm_box = CmFrame()
         gc_box = GcFrame()
         rc_box = RcFrame()
         tr_box = TrFrame()
         en_box = EnFrame()
 
-        stack.add_titled(ln_box, "ln", "Sequence Length")
+        stack.add_titled(sl_box, "sl", "Sequence Length")
         stack.add_titled(cm_box, "cm", "Sequence Comparison")
         stack.add_titled(gc_box, "gc", "Calculate GC")
         stack.add_titled(rc_box, "rc", "Reverse Complement")
