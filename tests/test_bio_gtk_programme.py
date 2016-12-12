9035daa454ac8b86297552c52d5f7cf4cbeb5b21
Fliss Tournant <flisstee@hotmail.co.uk>
2016-12-12 13:51:39 +0000

    Deleted files

--- tests/test_bio_gtk_programme.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from bio_gtk_programme import LnFrame, seq_len
-
-class TestSL(unittest.Testcase):
-	def test_seq_len(self):
-		self.assertTrue(seq_len >0)
-
-if __name__ == '__main__':
-	unittest.main()
